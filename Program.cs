c526032814a7849d8afd2586148c2871f088030c
marquezej <estemarquez@gmail.com>
2021-12-31 15:02:09 -0600

    clean

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-int previous = 0;
-
-int current = 0;
-
-int next = 1;
+Int64 previous = 0;
+Int64 current = 0;
+Int64 next = 1;
 
 app.MapGet("/",  () =>
     {
@@ -39,4 +37,19 @@ app.MapGet("/next",  () =>
         return current;
     }
 );
+
 app.Run();
+
+
+int GetNthFibonacci_Ite(int n)  
+{  
+    int number = n - 1; //Need to decrement by 1 since we are starting from 0  
+    int[] Fib = new int[number + 1];  
+    Fib[0]= 0;  
+    Fib[1]= 1;  
+    for (int i = 2; i <= number;i++)  
+    {  
+       Fib[i] = Fib[i - 2] + Fib[i - 1];  
+    }  
+    return Fib[number];  
+}
